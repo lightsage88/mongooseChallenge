3bed5f4fbe90391bd40372dff13b062214fef698
lightsage88 <adrian.e.rosales@gmail.com>
2017-11-28 21:03:13 -0800

    reversion

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/glappy';
 
 exports.TEST_DATABASE_URL = (
-	process.env.TEST_DATABASE_URL ||'mongodb://localhost/127.0.0.1:27017/test' ||'mongodb://localhost/test-glappy');
+	process.env.TEST_DATABASE_URL ||'mongodb://localhost/test-glappy');
 exports.PORT = process.env.PORT || 8080;
